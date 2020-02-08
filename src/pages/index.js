8fb4aad3b703eb2723cb5d3e2657e8b68b1a4ef7
Timothy Shores <timothymshores@gmail.com>
2020-02-08 12:09:12 -0600

    Run gatsby new from cli, npm install pure-react-carousel by express labs and add vertical carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,34 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+  DotGroup,
+} from "pure-react-carousel"
+import "pure-react-carousel/dist/react-carousel.es.css"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
+    <CarouselProvider
+      naturalSlideWidth={100}
+      naturalSlideHeight={25}
+      totalSlides={3}
+      orientation={`vertical`}
+    >
+      <Slider>
+        <Slide index={0}>I am the first Slide.</Slide>
+        <Slide index={1}>I am the second Slide.</Slide>
+        <Slide index={2}>I am the third Slide.</Slide>
+      </Slider>
+      <ButtonBack>Back</ButtonBack>
+      <ButtonNext>Next</ButtonNext>
+    </CarouselProvider>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
